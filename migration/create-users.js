60fd59109b87e979b1281badc4b0b216a6cc3dcc
Daniil <pde-rnd>
2021-03-28 17:36:49 +0300

    DB splitted into 2 tables: users and logged_users

--- migration/create-users.js
+++ migration/create-users.js
@@ -6,15 +6,24 @@ const db = postgres({
 });
 
 const argv = require('process').argv;
+(async () => {
 if (argv[2] == 'up') {
-    db`CREATE TABLE users(
+    await db`CREATE TABLE IF NOT EXISTS users (
         id serial PRIMARY KEY NOT NULL,
         name VARCHAR(100) NOT NULL,
-        password VARCHAR(32) NOT NULL,
-        token CHAR(32)
+        password VARCHAR(32) NOT NULL
     )`
+    await db`CREATE TABLE IF NOT EXISTS logged_users (
+        token CHAR(32) PRIMARY KEY NOT NULL,
+        user_id integer REFERENCES users NOT NULL
+    )`
+    console.log("Migrated");
 } else if (argv[2] == 'down') {
-    db`DROP TABLE users`
+    await db`DROP TABLE IF EXISTS logged_users`
+    await db`DROP TABLE IF EXISTS users`
+    console.log("Migrated");
 } else {
     throw new Error("Invalid argument, expected 'up' or 'down'")
 }
+process.exit(0)
+})()
