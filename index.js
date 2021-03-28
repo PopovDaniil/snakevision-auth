525ffbb99cadc00c2e30d215b685ae455d67cd84
Daniil <pde-rnd>
2021-03-28 21:29:22 +0300

    Added processing of POST /reg

--- index.js
+++ index.js
@@ -1,19 +1,51 @@
 const fastify = require('fastify').fastify()
-const postgres = require('postgres')
+const users = require('./users').users
 
-const db = postgres({
-    host: process.env.AUTH_DB_HOST,
-    username: process.env.AUTH_DB_USER,
-    password: process.env.AUTH_DB_PASSWORD
-});
-
-// (async () => {
-//     const res = await db`select * from users`
-//     console.log(res)
-// })()
+const headersSchema = {
+    type: 'object',
+    properties: {
+        "Auth-Token": { type: 'string' }
+    },
+    required: ['Auth-Token']
+}
 
 fastify
-.get("/", (req, res) => {
-    res.send("Hello")
-})
-.listen(process.env.AUTH_PORT, '0.0.0.0')
+    .get("/", (request, reply) => {
+        reply.send("Hello")
+    })
+    .post("/reg",
+        {
+            schema: {
+                body: {
+                    type: 'object',
+                    properties: {
+                        name: { type: 'string' },
+                        login: { type: 'string' },
+                        age: { type: 'number' },
+                        login: { type: 'string' },
+                        email: { type: 'string' },
+                        telephone: { type: 'number' },
+                        password: { type: 'string' }
+                    },
+                    required: ['name', 'login', 'age', 'email', 'telephone', 'password']
+                },
+                response: {
+                    200: {
+                        type: 'object',
+                        properties: {
+                            login: { type: 'string' },
+                            email: { type: 'string' },
+                            telephone: { type: 'number' },
+                        },
+                        required: ['login','email','telephone']
+                    }
+                }
+            }
+        },
+        async (request, reply) => {
+            const status = await users.add(request.body)
+            reply.code(200)
+            reply.send(status[0])
+        }
+    )
+    .listen(process.env.AUTH_PORT, '0.0.0.0')
