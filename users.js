816c6be7703f31d2f67eda000fdbe27e3db671ec
Daniil <pde-rnd>
2021-03-29 19:29:33 +0300

    Added login method

--- users.js
+++ users.js
@@ -1,4 +1,5 @@
 const postgres = require('postgres')
+const crypto = require('crypto')
 
 const sql = postgres({
     host: process.env.AUTH_DB_HOST,
@@ -8,13 +9,32 @@ const sql = postgres({
 
 const Users = {
     async add(user) {
-        const {name, login, age, email, telephone, password} = user
-        let res = await sql`
+        const { name, login, age, email, telephone, password } = user
+        const res = await sql`
             INSERT INTO users (name, login, age, email, telephone, password)
             VALUES (${name},${login},${age},${email},${telephone},md5(${password}))
             RETURNING login, email, telephone`
-            .catch(e => {throw new Error(e)})
+            .catch(e => { throw new Error(e) })
         return res;
+    },
+
+    async login(opts) {
+        const {login, password} = opts
+        const user = await sql`
+            SELECT id FROM users 
+            WHERE login = ${login} 
+            AND password = md5(${password})`
+            .catch(e => { throw new Error(e) })
+        if (!user.count) {
+            return false;
+        }
+        const userId = user[0].id
+        const token = crypto.randomBytes(16).toString('hex')
+        console.log(userId);
+        await sql`
+            INSERT INTO logged_users VALUES (${token},${userId})`
+            .catch(e => { throw new Error(e) })
+        return token
     }
 }
 
