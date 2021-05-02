eccef7885f0407e3ccd6ea9862eaaa62197e966c
Daniil <pde-rnd@yandex.ru>
2021-05-02 19:09:00 +0300

    Deprecated method names were removed

--- test/app.js
+++ test/app.js
@@ -11,7 +11,7 @@ t.test('App working', async t => {
         method: 'GET',
         url: '/'
     })
-    t.strictEqual(res.statusCode,200,'returns code 200')
-    t.strictEqual(res.body,"Hello","'Hello' string received")
-    t.strictEqual(res.headers["access-control-allow-origin"],"*", "CORS origin is set to *")
+    t.equal(res.statusCode,200,'returns code 200')
+    t.equal(res.body,"Hello","'Hello' string received")
+    t.equal(res.headers["access-control-allow-origin"],"*", "CORS origin is set to *")
 })
